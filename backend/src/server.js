423d5aef2731f146191ce6bebbf0c3fdd4e44d05
Siddaji <siddaji874@gmail.com>
2025-10-15 21:13:45 +0530

    Setup Backend with MongoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
 const app = express()
 const port = 3000
+
+dotenv.config();
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
